1c97dd6e0d8cd29a82ba134ab688ceb0a84669fb
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-02-09 14:15:09 +0100

    ex0 done!

---
+++ ex0/construct.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import site
+
+
+def main() -> None:
+    try:
+        env_path = os.environ.get("VIRTUAL_ENV")
+        current_py = sys.executable
+        current_env = os.path.basename(env_path)
+        print("\nMATRIX STATUS: Welcome to the construct\n")
+        print(f"Current Python: {current_py}")
+        print(f"Virtual Environnement: {current_env}")
+        print(f"Environnement Path: {env_path}\n")
+        print("SUCCESS: You're in an isolated environment!")
+        print("Safe to install packages without affecting")
+        print("the global system.")
+        print("\nPackage installation path:")
+        print(f"{site.getsitepackages()[0]}")
+    except Exception:
+        print("\nMATRIX STATUS: You're still plugged in\n")
+        print(f"Current Python: {current_py}")
+        print("Virtual Environnement: None detected\n")
+        print("WARNING: You're in the global environment!")
+        print("The machines can see everything you install.")
+        print("\nTo enter the construct, run:")
+        print("python -m venv matrix_env")
+        print("source matrix_env/bin/activate # on Unix")
+        print("matrix_env")
+        print("Scripts")
+        print("activate     # On Windows")
+        print("\nThen run this program again.")
+
+
+if __name__ == "__main__":
+    main()
